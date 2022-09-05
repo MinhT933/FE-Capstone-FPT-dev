e459d39395c54b396078ce911d54c16f5642ef29
MinhT <phammanhtoanhht933@gmail.com>
2022-09-05 23:49:58 +0700

    update Page 404 and fix any thing

--- src/App.js
+++ src/App.js
@@ -5,33 +5,9 @@ import { Route } from "react-router-dom";
 import routesHome from "./route.js";
 import Router from "./route.js";
 function App() {
-  // const showMenuHome = (routes) => {
-  //   if (routes && routes.length > 0) {
-  //     return routes.map((item, index) => {
-  //       const Component = () => {
-  //         return item.component;
-  //       }
-  //       // const Component = item.component;
-  //       return (
-  //         <Route
-  //           path={item.path}
-  //           key={index}
-  //           index={item.index}
-  //           element={
-  //             <HomeLayout>
-  //               <Component />
-  //             </HomeLayout>
-  //           }
-  //         />
-  //       );
-  //     });
-  //   }
-  // };
+
   return (
     <BrowserRouter>
-      {/* <Routes>
-        {showMenuHome(Routes)}
-      </Routes> */}
       <Router/>
     </BrowserRouter>
   );
--- src/route.js
+++ src/route.js
@@ -5,15 +5,16 @@ import User from './pages/user/User';
 import ProductList from './pages/Product/ProductList';
 import { useRoutes } from 'react-router-dom'
 import DashboardLayout from './layouts/DashboardLayout.js';
+import Page404 from './pages/NotFound/Page404';
 
 
 export default function Router(){
     return useRoutes([
-        // {
-        //     path: '/',
-        
-        //     children:[{path:'/', element:<Home/>}]
-        // },
+        {
+            path: '/',
+            element:<DashboardLayout/>,
+            children:[{path:'/', element:<Home/>}]
+        },
         
         {
           path: '/dashboard',
@@ -22,6 +23,10 @@ export default function Router(){
             { path: 'app', element: <Home /> },
             { path: 'users', element: <UserList /> },
             { path: 'product', element: <ProductList /> },
+            { path: '404', element: <Page404/> },
+            { path: 'blog', element: <Page404/> },
+            { path: 'login', element: <Page404/> },
+            { path: 'register', element: <Page404/> },
           ],
         }]);
 }
