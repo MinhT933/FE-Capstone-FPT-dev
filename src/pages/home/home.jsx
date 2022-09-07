b069da1acf9d7c305a4b6d9bcfe16ee77e39f0f1
MinhT <phammanhtoanhht933@gmail.com>
2022-09-08 01:07:00 +0700

    update  newpackage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./home.css";
+
 
 export default function Home() {
   return (
     <div className="home">
       <div className="homeWidgets">
-         <h1>Nàng vô tình trao ta một nụ cười </h1>
-         <h1>Ta  đa tình tưởng đó là yêu</h1>
+         <h1>Dù quên đi tên một loài hoa, người ta vẫn biết hoa đẹp ở điểm nào</h1>
+        <img src='https://cdn.voh.com.vn/voh/thumbnail/2019/10/18/chonstatusvehoahaynhatdemuonhoatotinhvoinguoithuongvoh5_20191018092903.jpg'></img>
       </div>
     </div>
   );
