7cf8060c15b537d320282d63ef02e40cf5ccd5fb
MinhT933 <61859288+MinhT933@users.noreply.github.com>
2022-09-18 19:00:13 +0700

    Update UpdateService.jsx

--- src/utils/UpdateService/UpdateService.jsx
+++ src/utils/UpdateService/UpdateService.jsx
@@ -6,15 +6,12 @@ export const getOptions = () => [
 ];
 
 
-export const getOptionsGroup = () => ([
-
+export const getOptionsGroup = () => [
   { id: "1", title: "gói cao cấp" },
   { id: "2", title: "gói trung cấp" },
   { id: "3", title: "gói bình dân" },
   { id: "4", title: "gói dành cho người ăn chay" },
-
-];)
-
+];
 
 export const Cate = () => [
   { id: "1", title: "Chay" },
