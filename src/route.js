6a10eecd1c687295dc64006268f2c1ba311c5b02
MinhT <phammanhtoanhht933@gmail.com>
2022-09-22 14:33:33 +0700

    fix conflit

--- src/route.js
+++ src/route.js
@@ -15,23 +15,14 @@ import SignInOutContainer from "./pages/Login/index.js";
 import VerifyPhone from "./pages/Login/VerifyPhone.js";
 import ChangePassword from "./pages/Login/ChangePassword.js";
 
-import TabsPacket from "./pages/PackageFood/Tabs/ColorTabs.js";
-
 import StationList from "./pages/Station/StationList";
 import NewStation from "./pages/Station/NewStation.jsx";
-
-
 import KitchenList from "./pages/Kitchen/KitchenList";
 import NewKitchen from "./pages/Kitchen/NewKitchen";
-
-
 import AdminOrderList from "./pages/AdminOrder/AdminOrderList";
 import NewAdminOrder from "./pages/AdminOrder/NewAdminOrder";
-import TabsPacket from "./pages/PackageFood/Tabs/ColorTabs.js";
-import ManagerFoodGroup from "./pages/FoodGroup/ManagerFoodGroup.jsx";
 import StepDesignPacketFood from "./pages/PackageFood/Step/StepDesignPacketFood.jsx";
 import ListFoodGroup from "./pages/FoodGroup/ListFoodGroup.jsx";
-
 
 export default function Router() {
   return useRoutes([
@@ -77,10 +68,8 @@ export default function Router() {
         { path: "station", element: <StationList /> },
         { path: "newstation", element: <NewStation /> },
 
-
         { path: "kitchen", element: <KitchenList /> },
         { path: "newkitchen", element: <NewKitchen /> },
-
 
         { path: "adminorder", element: <AdminOrderList /> },
         { path: "newadminorder", element: <NewAdminOrder /> },
@@ -95,10 +84,8 @@ export default function Router() {
       children: [
         { path: "kitchen", element: <KitchenList /> },
         { path: "newkitchen", element: <NewKitchen /> },
-
       ],
     },
-
 
     // {
     //   path: "/dashboard/kitchen",
