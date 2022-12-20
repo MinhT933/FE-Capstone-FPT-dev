37874dfdd9863438e1b44f913526c2eed449721e
MinhT <phammanhtoanhht933@gmail.com>
2022-12-21 01:21:55 +0700

    url local host

--- src/Axios/URL_API/URL.jsx
+++ src/Axios/URL_API/URL.jsx
@@ -1,6 +1,6 @@
 // export const URL_API = `https://mesup.herokuapp.com`;
-export const URL_API = `http://14.225.205.162:2004`;
-
+// export const URL_API = `http://14.225.205.162:2004`;
+export const URL_API = `http://localhost:3001`;
 // URL_API + "/foods" = http://localhost:3001/foods
 // URL_API + "/foods" = http://localhost:3001/foods
 // URL_API + "/foods" = http://localhost:3001/foods
